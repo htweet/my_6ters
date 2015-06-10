d4aa042421982383825828883fb6994deae06a86
Banji Oyebola <hemrontweet@gmail.com>
2015-06-10 21:08:52 +0100

    Add a Static Pages controller

---
+++ test/controllers/static_pages_controller_test.rb
@@ -0,0 +1,14 @@
+require 'test_helper'
+
+class StaticPagesControllerTest < ActionController::TestCase
+  test "should get home" do
+    get :home
+    assert_response :success
+  end
+
+  test "should get helper" do
+    get :helper
+    assert_response :success
+  end
+
+end

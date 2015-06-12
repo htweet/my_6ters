2f7107bd1ccce60fc30cb6ebc0957eff0edc9592
Banji Oyebola <hemrontweet@gmail.com>
2015-06-12 15:08:01 +0100

    Finish layout and routes

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -3,20 +3,25 @@ require 'test_helper'
 class StaticPagesControllerTest < ActionController::TestCase
   test "should get home" do
     get :home
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
     assert_response :success
   end
 
   test "should get help" do
     get :help
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
     assert_response :success
+    assert_select "title", "Help | 6ters"
   end
 
   test "should get about" do
     get :about
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
     assert_response :success
+    assert_select "title", "About | 6ters"
+  end
+
+  test "should get contact" do
+    get :contact
+    assert_response :success
+    assert_select "title", "Contact | 6ters"
   end
 	
 end

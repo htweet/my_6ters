1959c5384f6145d1ad77daf9fac3f05dae7e3ca6
Banji Oyebola <hemrontweet@gmail.com>
2015-06-15 21:04:25 +0100

    Add account activations

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,6 +11,8 @@ User.create!(name:  "Example User",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true)
+             activated: true,
+             activated_at: Time.zone.now)
 
 99.times do |n|
   name  = Faker::Name.name
@@ -20,4 +22,6 @@ User.create!(name:  "Example User",
                email: email,
                password:              password,
                password_confirmation: password)
+               activated: true,
+               activated_at: Time.zone.now)
 end
